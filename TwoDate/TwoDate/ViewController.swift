c9f306ea8e1cb13ad0a3687611ae88045c13c997
Jeannie Kinnett <Jean@jmk-computer.local>
2016-04-04 19:43:52 -0400

    Login Screen.

--- TwoDate/TwoDate/ViewController.swift
+++ TwoDate/TwoDate/ViewController.swift
@@ -11,6 +11,35 @@ import SlideMenuControllerSwift
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var menButton: UIButton!
+    @IBOutlet weak var womenButton: UIButton!
+    
+    var preferMen = false
+    var preferWomen = false
+    
+    @IBAction func menButtonTapped(sender: AnyObject) {
+        if preferMen == false {
+            menButton.setImage(UIImage(named: "menChecked"), forState:.Normal)
+            preferMen = true
+        } else {
+            menButton.setImage(UIImage(named: "menUnchecked"), forState:.Normal)
+            preferMen = false
+        }
+        
+    }
+    
+    @IBAction func womenButtonTapped(sender: AnyObject) {
+        if preferWomen == false {
+            womenButton.setImage(UIImage(named: "womenChecked"), forState:.Normal)
+            preferWomen = true
+        } else {
+            womenButton.setImage(UIImage(named: "womenUnchecked"), forState:.Normal)
+            preferWomen = false
+        }
+    
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
